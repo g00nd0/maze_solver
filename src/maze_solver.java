b45d351971bc2a77715a63cf872d9e321f3c7798
Mitch Goon <28462666+g00nd0@users.noreply.github.com>
2020-04-30 23:50:46 +0800

    Completed Day 16 - Looping with File Input

--- src/maze_solver.java
+++ src/maze_solver.java
@@ -19,24 +19,8 @@ public class maze_solver {
 
     public static void main(String[] args) throws FileNotFoundException {
  
-        ArrayList<Maze> mazes = new ArrayList<Maze>(); //array list created so thay app can solve multiple mazes, saved in array list
-        
-        Maze m = new Maze();
-
-        //fill list from file
-        Scanner in = new Scanner(new File("mazes.txt")); //takes file input from Scanner
-        int rows = Integer.parseInt(in.nextLine()); //checks number of rows in input file, in this case it is defined in at the first line of txt file
-        m.maze = new int[rows][]; //initialse rows
-
-        for(int i = 0; i < rows; i++){ //iterate thru file, row by row
-            String line = in.nextLine(); //new String type to read array as a line
-            m.maze[i] = Arrays.stream(line.split(", ")).mapToInt(Integer::parseInt).toArray(); // read each element of array, convert to int, after ","
-        }
-
-        m.start = new Position(Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine())); //define start position from txt file 
-    
-        mazes.add(m); // adding maze from txt file to array list
-
+        ArrayList<Maze> mazes = readMazes();
+       
         int i = 0;
         while(i < mazes.size()){    // this loop goes executes the number of iterations equal to the size of 'mazes' array list
             if(solve_maze(mazes.get(i))) {   // refactored, mazes.get(i) gets the maze based on the index passed in from while loop
@@ -125,5 +109,36 @@ public class maze_solver {
             return false;
         }
         return true;
+    }
+
+    private static ArrayList<Maze> readMazes() throws FileNotFoundException {   //separate method to refactor the parsing of maze from text file
+        ArrayList<Maze> mazes = new ArrayList<Maze>(); //array list created so thay app can solve multiple mazes, saved in array list
+       
+        Scanner in = new Scanner(new File("mazes.txt")); //takes file input from Scanner
+
+        while(in.hasNext()){
+            
+            Maze m = new Maze();
+
+            //fill list from file
+           
+            int rows = Integer.parseInt(in.nextLine()); //checks number of rows in input file, in this case it is defined in at the first line of txt file
+            
+            m.maze = new int[rows][]; //initialse rows
+    
+            for(int i = 0; i < rows; i++){ //iterate thru file, row by row
+                String line = in.nextLine(); //new String type to read array as a line
+                m.maze[i] = Arrays.stream(line.split(", ")).mapToInt(Integer::parseInt).toArray(); // read each element of array, convert to int, after ","
+            }
+    
+            m.start = new Position(Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine())); //define start position from txt file 
+        
+            in.nextLine(); //from txt file, end of set of data has a space
+
+            mazes.add(m); // adding maze from txt file to array list
+        }
+        
+
+        return mazes;
     }
 }
